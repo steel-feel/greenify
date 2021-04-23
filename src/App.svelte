32fdeb11b491d1810c36ad81f7cf925be3f5c9c5
Steel Feel <himankjain@yahoo.in>
2021-04-23 23:25:52 +0530

    grid cards intigration

--- src/App.svelte
+++ src/App.svelte
@@ -6,17 +6,92 @@
 
 <main>
 	<Header />
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
+
+<div class="parent ">
+	<div class="div1 "> 
+	   <div class="shadow-lg rounded-2xl p-4 bg-white dark:bg-gray-800">
+	   <div class="mx-1 flex items-center">
+			<span class="rounded-xl relative p-4 bg-green-200">
+				<svg width="40" fill="currentColor" height="40" class="text-green-500 h-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+					<path d="M1362 1185q0 153-99.5 263.5t-258.5 136.5v175q0 14-9 23t-23 9h-135q-13 0-22.5-9.5t-9.5-22.5v-175q-66-9-127.5-31t-101.5-44.5-74-48-46.5-37.5-17.5-18q-17-21-2-41l103-135q7-10 23-12 15-2 24 9l2 2q113 99 243 125 37 8 74 8 81 0 142.5-43t61.5-122q0-28-15-53t-33.5-42-58.5-37.5-66-32-80-32.5q-39-16-61.5-25t-61.5-26.5-62.5-31-56.5-35.5-53.5-42.5-43.5-49-35.5-58-21-66.5-8.5-78q0-138 98-242t255-134v-180q0-13 9.5-22.5t22.5-9.5h135q14 0 23 9t9 23v176q57 6 110.5 23t87 33.5 63.5 37.5 39 29 15 14q17 18 5 38l-81 146q-8 15-23 16-14 3-27-7-3-3-14.5-12t-39-26.5-58.5-32-74.5-26-85.5-11.5q-95 0-155 43t-60 111q0 26 8.5 48t29.5 41.5 39.5 33 56 31 60.5 27 70 27.5q53 20 81 31.5t76 35 75.5 42.5 62 50 53 63.5 31.5 76.5 13 94z">
+					</path>
+				</svg>
+			</span>
+			<p class="text-md text-black dark:text-white ml-2">
+				Green Token
+			</p>
+		</div>
+	  
+	  <div class="flex flex-row place-content-center items-center"  >
+	  <h1 class="text-xl mx-5" >Balance</h1> 
+	  <div>
+		<div class="flex flex-col justify-start">
+			<p class="text-gray-700 dark:text-gray-100 text-4xl text-left font-bold my-4">
+				50
+				<span class="text-sm">
+					GTK
+				</span>
+			</p>
+			<div class="flex items-center text-green-500 text-sm">
+				<svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+					<path d="M1408 1216q0 26-19 45t-45 19h-896q-26 0-45-19t-19-45 19-45l448-448q19-19 45-19t45 19l448 448q19 19 19 45z">
+					</path>
+				</svg>
+				<span>
+					5.5% 
+				</span>
+				<span class="mx-1 text-gray-400">
+					vs last month
+				</span>
+			</div>
+		</div>
+	  </div>
+	  </div>
+	</div>
+	  </div>
+	<div class="div2 red">  Offers   </div>
+	<div class="div3 black"> Shop</div>
+	<div class="div4 ">
+	<!--  <div class="flex-1 place-content-center" > -->
+	<!-- <div class="self-center" > -->
+	  <button type="button" class="max-w-lg  py-2 px-16  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+		Get reward
+	</button> 
+	<!--   </div>  -->
+	  
+	<!--    </div>  -->
+	  </div>
+	</div>
+	
 </main>
-<style global>
-	@tailwind base;
-	@tailwind components;
-	@tailwind utilities;
 
-	@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+<style>
+.parent {
+display: grid;
+grid-template-columns: repeat(2, 1fr);
+grid-template-rows: repeat(3, 1fr);
+grid-column-gap: 0px;
+grid-row-gap: 0px;
+place-items: center;
+  
+}
 
-	body{
-		  font-family: 'Poppins', sans-serif;
-	}
+.div1 { grid-area: 1 / 1 / 2 / 3; width: 90%; }
+.div2 { grid-area: 2 / 1 / 3 / 2; }
+.div3 { grid-area: 2 / 2 / 3 / 3; }
+.div4 { grid-area: 3 / 1 / 4 / 3; }
+
+.red {
+ background-color: red;
+}
+.yellow {
+ background-color: yellow;
+}
+.black {
+ background-color: yellow;
+}
+.green {
+background-color: green;
+}
 </style>
--- src/Header.svelte
+++ src/Header.svelte
@@ -20,16 +20,16 @@ function handleClick (){
 </style>
 
 
-<nav class="flex items-center justify-between flex-wrap bg-gray-200 p-6">
+<nav class="flex items-center justify-between flex-wrap bg-gray-200 p-4">
     <div class="flex items-center flex-shrink-0 text-black mr-6">
       <!-- <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> -->
       <img src="./logo.png" class=" h-8 w-8 mr-2" alt="">
       <span class="font-semibold text-xl tracking-tight">Greenify</span>
     </div>
-    <div class="block md:hidden">
-      <button on:click={handleClick}  class="cursor-pointer flex items-center px-3 py-2 border rounded text-black-100 border-gray-200 ">
-        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-      </button>
+    <div class="block md:hidden ">
+      <a on:click={handleClick}  class="cursor-pointer flex items-center px-3 py-2 border rounded text-black-100 border-gray-200 ">
+        <svg class="fill-current h-4 w-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </a>
     </div>
     <div id="mobile-menu" class:hideclass="{m}" class="fixed right-0 top-0 h-screen my-1 w-2/3 z-2 block flex-grow md:flex bg-gray-500 md:justify-end md:w-auto">
         <a on:click={handleClick}  class="float-right clear-right my-3 cursor-pointer flex items-center px-3 py-2 text-white border-gray-200 ">
@@ -38,9 +38,9 @@ function handleClick (){
               </svg>
             </a>
         <div>
-        <a href="#" on:click={handleClick} class="block md:inline-block text-md px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500  mt-4 md:mt-0">Home</a>
-         <a href="#"  on:click={handleClick} class="block md:inline-block text-md px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500  mt-4 md:mt-0">How-tos</a>
-         <a href="#" on:click={handleClick}  class="block md:inline-block text- px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500  mt-4 md:mt-0">Trasaction History</a>
+        <a href="#" on:click={handleClick} class="font-bold block md:inline-block text-md px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500  mt-4 md:mt-0">Home</a>
+         <a href="#"  on:click={handleClick} class="font-bold block md:inline-block text-md px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500  mt-4 md:mt-0">How-tos</a>
+         <a href="#" on:click={handleClick}  class="font-bold block md:inline-block text- px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500  mt-4 md:mt-0">Trasaction History</a>
       </div>
     </div>
   </nav>
